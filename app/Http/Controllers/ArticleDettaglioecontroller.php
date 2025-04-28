d1331c7c8c272815e22ccb6d59a28dd153e10ca2
Beatrice Errico <1.beatriceerrico@gmail.com>
2025-04-28 16:14:20 +0200

    AGGIUNTI DATABASE

---
+++ app/Http/Controllers/ArticleDettaglioecontroller.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Article;
+use Illuminate\Http\Request;
+
+class ArticleDettaglioecontroller extends Controller
+{
+    public function show (string $id){
+        $article= Article::findorfail($id);
+
+        return view('details.dettagli',compact('article'));
+    }
+}
---
+++ resources/views/details/dettagli.blade.php
@@ -0,0 +1,13 @@
+<x-main-layout>
+<x-slot:title>Dettaglio</x-slot:title>
+
+
+  <!-- HERO-SECTION -->
+  <x-hero-section title="" subtitle="Scopri il mondo delle auto con articoli, recensioni e storie su quattro ruote. Il viaggio comincia qui." />
+
+
+  <!-- CONTENUTO PAGINA -->
+<x-article-dettaglio-card :article="$article" />
+
+
+</x-main-layout>
